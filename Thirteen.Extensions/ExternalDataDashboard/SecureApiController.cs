281a316362a99fe0dd9b19582a082986cc0afadb
Jesper Mayntzhusen <jm@ecreo.dk>
2024-11-24 16:17:12 +0100

    Calling authorized endpoint v13

---
+++ Thirteen.Extensions/ExternalDataDashboard/SecureApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+
+namespace Thirteen.Extensions.ExternalDataDashboard;
+
+public class SecureApiController : UmbracoAuthorizedApiController 
+{
+    [HttpGet]
+    public string GetMessage() 
+    {
+        return "This is a message from a secure API controller!";
+    }
+}
---
+++ Thirteen.Extensions/wwwroot/ExternalDataDashboard/externalDataDashboard.controller.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    function Controller($http) {
+
+        var vm = this;
+        vm.message = "";
+
+        $http.get("backoffice/api/SecureApi/GetMessage").then(function (response) {
+            vm.message = response.data;
+        });
+    }
+
+    angular.module("umbraco").controller("ExternalDataDashboard.Controller", Controller);
+
+})();
